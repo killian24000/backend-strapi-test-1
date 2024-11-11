ce56898ca125317152fc314b6955d988c8040beb
Killian PERRICHET <102808884+killian24000@users.noreply.github.com>
2024-11-11 10:56:17 +0100

    feat(collection): add project

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:4200'], // L'URL de ton application Angular
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
